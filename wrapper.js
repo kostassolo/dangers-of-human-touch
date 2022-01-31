5fd885fb2eff02de13780be30e7f9af0e566c3c4
kostassolo <ksolom6@uic.edu>
2022-01-31 13:32:44 -0600

    functions

---
+++ wrapper.js
@@ -0,0 +1,19 @@
+
+// All the mouse and key events
+Events = new Set(['Keydown','Keyup','Keypress','Scroll','Mousewheel','Wheel','Doubleclick','Select','Click','Mousedown','Mouseup','Blur','Focus','Cut','Copy','Paste','Mouseenter','Mouseout','Mousemove','Mouseover'])
+orig = EventTarget.prototype.addEventListener;
+EventTarget.prototype.addEventListener = function(){
+  if ( Events.has(arguments[0]) ){
+    let handler = arguments[1]
+    arguments[1] = function(){
+       let event = arguments[0];
+       //event's origin
+       if (event.isTrusted == false)
+         return;
+       else
+         return handler.apply(this,arguments)
+     }
+   }
+  return orig.apply(this, arguments);
+
+}
